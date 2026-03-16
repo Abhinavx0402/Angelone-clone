98673c92516f086f5ab4cb77cd2da6471151fff9
Abhinav <abhinavusictit@gmail.com>
2026-03-16 16:30:02 +0530

    new files added

---
+++ script.js
@@ -0,0 +1,29 @@
+new TradingView.widget({
+
+    "container_id": "tradingview_chart",
+    
+    "width": "100%",
+    
+    "height": "500",
+    
+    "symbol": "NSE:RELIANCE",
+    
+    "interval": "D",
+    
+    "timezone": "Asia/Kolkata",
+    
+    "theme": "light",
+    
+    "style": "1",
+    
+    "locale": "en",
+    
+    "toolbar_bg": "#f1f3f6",
+    
+    "enable_publishing": false,
+    
+    "hide_top_toolbar": false,
+    
+    "save_image": false
+    
+    });
